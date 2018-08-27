c5185e73686aa8e4f94f40ca0e657b8fcf0412db
Felix Lisczyk <felix.lisczyk@gmail.com>
2018-08-27 13:36:31 +0200

    Narrowed regex for list element

--- Source/CDMarkdownList.swift
+++ Source/CDMarkdownList.swift
@@ -33,7 +33,7 @@
 
 open class CDMarkdownList: CDMarkdownLevelElement {
 
-    fileprivate static let regex = "^\\s*([\\*\\+\\-]{1,%@})[ \t]+(.+)$"
+    fileprivate static let regex = "^(\\*{1,%@})[ \t]{1}(\\w+)$"
 
     open var maxLevel: Int
     open var font: CDFont?
