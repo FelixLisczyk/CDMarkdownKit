372126ef5fd1df4f1241997aec6a190cad59c955
Felix Lisczyk <5102728+FelixLisczyk@users.noreply.github.com>
2025-07-17 10:22:01 +0200

    Revert "Don't add link attribute to images"
    This reverts commit bc9a624e5f963e5e8ea811fd899951b718f98449.

--- Source/CDMarkdownImage.swift
+++ Source/CDMarkdownImage.swift
@@ -66,7 +66,15 @@ open class CDMarkdownImage: CDMarkdownLinkElement {
 
     open func formatText(_ attributedString: NSMutableAttributedString,
                          range: NSRange,
-                         link: String) {}
+                         link: String) {
+        guard let encodedLink = link.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlHostAllowed)
+            else {
+                return
+        }
+        guard let url = URL(string: link) ?? URL(string: encodedLink) else { return }
+
+        attributedString.addAttribute(.link, value: url, range: range)
+    }
 
     open func match(_ match: NSTextCheckingResult,
                     attributedString: NSMutableAttributedString) {
